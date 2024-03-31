2a3a24c2ad3586ee865d45791ee2cfa868649136
Makhruf <161798155+Makhruf@users.noreply.github.com>
2024-03-31 16:44:38 +0700

    Frontend code pertemuan 3

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import Layout from "../component/Layout";
+import Utama from "../component/Utama";
+
+function Home() {
+    return ( 
+        <>
+        <Layout>
+            <Utama/>
+        </Layout>
+        </>
+     );
+}
+
+export default Home;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import Contoh from './contoh.jsx'
+import './index.css'
+import Utama from './component/Utama.jsx'
+import IndukAyam from './Pages/Induk-ayam.jsx'
+import Belajar from './Belajar.jsx'
+import Navbar from './component/Navbar.jsx'
+import Sidebar from './component/Sidebar.jsx'
+import Layout from './component/Layout.jsx'
+import Home from './Pages/Home.jsx'
+import { BrowserRouter } from 'react-router-dom'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App /> */}
-    <Contoh/> 
-  </React.StrictMode>,
+    {/* <Utama/> */}
+    {/* <Belajar/> */}
+    {/* <IndukAyam/> */}
+    {/* <Navbar/> */}
+    {/* <Sidebar/> */}
+    {/* <Layout/> */}
+    {/* <Home/> */}
 
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
+  </React.StrictMode>,
 )
